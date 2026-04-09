447d9e69f0667035d992c99d7098be906d18b1e1
Omkar Bajirao Pawar <91528431+098-omkar@users.noreply.github.com>
2026-04-09 12:33:43 +0530

    Create EasyLoanPortalApplication.java

---
+++ EasyLoanPortalApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyLoanPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyLoanPortalApplication.class, args);
+	}
+
+}
